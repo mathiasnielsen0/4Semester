18bbe898ee02d0d1cf0a85d11b41bc08c6b53004
Mathiasnielsen <au584107@uni.au.dk>
2019-02-19 16:14:08 +0100

    agent07

---
+++ AgentExercis07/Exercise2/AgentExercise/MainWindowViewModel.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using I4GUI;
+using Prism.Mvvm;
+
+namespace AgentExercise
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private ObservableCollection<Agent> _agents;
+
+        public MainWindowViewModel()
+        {
+            _agents = new ObservableCollection<Agent>
+            {
+                new Agent("001", "Nina", "Assasination", "UpperVolta"),
+                new Agent("007", "James Bond", "Assasination", "UpperVolta")
+            };
+        }
+
+        
+
+        public void LeftBtnCmd()
+        {
+
+        }
+
+        #region Properties
+
+        int currentIndex = -1;
+
+        public int CurrentIndex
+        {
+            get { return currentIndex; }
+            set { SetProperty(ref currentIndex, value); }
+        }
+
+        public ObservableCollection<Agent> Agents
+        {
+            get
+            {
+                return _agents;
+            }
+        }
+
+        private Agent _currentAgent = null;
+        public Agent CurrentAgent
+        {
+            get { return _currentAgent; }
+            set
+            {
+                SetProperty(ref _currentAgent, value);
+                //if (currentPerson != value)
+                //{
+                //    currentPerson = value;
+                //    NotifyPropertyChanged();
+                //}
+            }
+        }
+
+        #endregion
+    }
+}
+
+/*
+        private Agent Nina;
+        private Agent Bond;
+        private Agents AgentCollection = new Agents();
+
+    public MainWindow()
+        {
+            Nina = new Agent("001", "Nina", "Assasination", "UpperVolta");
+            Bond = new Agent("007", "James Bond", "Assasination", "UpperVolta");
+            InitializeComponent();
+            DataContext = AgentCollection;
+        }
+
+        private void AgentListBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            AgentCollection.Add(Nina);
+            AgentCollection.Add(Bond);
+        }
+
+        private void AddNewBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AgentCollection.Add(new Agent("N/A", "New Agent", "N/A", "N/A"));
+        }
+
+        private void leftBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AgentListBox.SelectedIndex -= 1;
+        }
+
+        private void rightBtn_Click(object sender, RoutedEventArgs e)
+        {
+            AgentListBox.SelectedIndex += 1;
+        }
+    }
+ */
